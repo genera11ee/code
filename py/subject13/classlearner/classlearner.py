4415ea6657285205c04f0e60d648ba4f76a625f6
genera11ee <sb.otto@icloud.com>
2020-04-25 15:56:30 +0200

    First version

---
+++ py/subject13/classlearner/classlearner.py
@@ -0,0 +1,17 @@
+class Sword:
+    weight = 0
+    size = 0
+    damage = 0
+    name = ""
+
+narsil = Sword()
+narsil.weight = 50
+narsil.size = 40
+narsil.name = "Narsil"
+narsil.damage = 30
+
+excalibur = Sword()
+excalibur.weight = 60
+excalibur.size = 30
+excalibur.name = "Excalibur"
+narsil.damage = 40
